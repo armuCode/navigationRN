6df12caa3beb3d7f40aa966375daa4e0ddb2a88f
armucode <armucode@gmail.com>
2024-02-26 06:22:48 -0500

    Se instalan los iconos de la app

--- src/screens/TabScreen1.tsx
+++ src/screens/TabScreen1.tsx
@@ -1,10 +1,13 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {View, Text} from 'react-native';
+import {styles} from '../theme/appTheme';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const TabScreen1 = () => {
   return (
-    <View>
-      <Text>Tab1Screen</Text>
+    <View style={styles.globalMargin}>
+      <Text>Iconos</Text>
+      <Icon name="at-circle-outline" size={100} color="#900" />
     </View>
   );
 };
